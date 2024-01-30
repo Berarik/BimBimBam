52d10e1d336db09cd987176bc8a4f54b8a08eba5
FGG <FGG@local>
2024-01-30 15:43:29 +0700

    1

--- BimBam/Program.cs
+++ BimBam/Program.cs
@@ -6,11 +6,22 @@ namespace BimBam
     {
         static void Main(string[] args)
         {
+            Random coin = new Random();
             string helloworld = "Hello, World!";
             if(helloworld.Length<10)
                 Console.WriteLine(helloworld);
             else
-                Console.WriteLine(helloworld[5]);
+                Console.WriteLine(helloworld[coin.Next(0, helloworld.Length)]);
+
+            int intN = 500;
+            Console.WriteLine($"Число: {(intN < 123 ? 1 : 2)}");
+            
+            int flipCoin = coin.Next(0, 2);
+            Console.WriteLine($"Выбопало:{ ((flipCoin == 0) ? "Orel" : "Reshka")}");
+            for (int i = 0;i<10;i++)
+            {
+                Console.WriteLine($"Выбопало (Номер попытки {i}): {((coin.Next(0, 2) == 0) ? "Orel" : "Reshka")} ");
+            }
 
         }
     }
