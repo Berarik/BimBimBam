24535af8153e7f04ba08dd3d49174247d74abead
FGG <FGG@local>
2024-01-30 12:44:37 +0700

    l2

--- BimBam/Program.cs
+++ BimBam/Program.cs
@@ -6,7 +6,12 @@ namespace BimBam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string helloworld = "Hello, World!";
+            if(helloworld.Length<10)
+                Console.WriteLine(helloworld);
+            else
+                Console.WriteLine(helloworld[5]);
+
         }
     }
 }
